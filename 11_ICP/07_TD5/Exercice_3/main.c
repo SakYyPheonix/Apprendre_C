037ef2a5fb328311c9b4593f4fc713fb10ae93e0
cbourgouin <charlybourgouin@gmail.com>
2021-02-03 10:49:27 +0100

    SAUVE TOI

--- 11_ICP/07_TD5/Exercice_3/main.c
+++ 11_ICP/07_TD5/Exercice_3/main.c
@@ -26,49 +26,65 @@ typedef struct {
 
 laStruct zoneCommune;
 
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+
 void *threadT1(void *arg) {
     laStruct val = *(laStruct *) arg;
     printf("pid dans le thread T1 : %d\n", getpid());
     printf("tid dans le thread T1 : %d\n", syscall(SYS_gettid));
+    pthread_mutex_lock(&mut);
     zoneCommune.v1 = val.v1;
     zoneCommune.v2 = val.v2;
     zoneCommune.v3 = val.v3;
-
+    pthread_mutex_unlock(&mut);
     pthread_exit((void *) "");
 }
 
 void *threadT2(void *arg) {
+    pthread_mutex_lock(&mut);
     laStruct val = *(laStruct *) arg;
     printf("pid dans le thread T2 : %d\n", getpid());
     printf("tid dans le thread T2 : %d\n", syscall(SYS_gettid));
+    pthread_mutex_lock(&mut);
     zoneCommune.v1 = val.v1;
     zoneCommune.v2 = val.v2;
     zoneCommune.v3 = val.v3;
-
+    pthread_mutex_unlock(&mut);
     pthread_exit((void *) "");
 }
 
 void *threadT3(void *arg) {
+    pthread_mutex_lock(&mut);
     laStruct val = *(laStruct *) arg;
     printf("pid dans le thread T3 : %d\n", getpid());
     printf("tid dans le thread T3 : %d\n", syscall(SYS_gettid));
+    pthread_mutex_lock(&mut);
     zoneCommune.v1 = val.v1;
     zoneCommune.v2 = val.v2;
     zoneCommune.v3 = val.v3;
-
+    pthread_mutex_unlock(&mut);
     pthread_exit((void *) "");
 }
 
 int main(int argc, char** argv) {
     int res;
     pthread_t t1, t2, t3;
+    laStruct str1, str2, str3, val;
+    res = pthread_mutex_init(&mut, NULL);
+    if (res != 0)
+    {
+        perror("Pb Mutex initialization");
+        exit(EXIT_FAILURE);
+    }
     
+    pthread_mutex_lock(&mut);
     zoneCommune.v1 = 0;
     zoneCommune.v2 = 0;
     zoneCommune.v3 = 0;
-    laStruct val;
+    printf("Zone Commune : v1=%d, v2=%d, v3=%d")
+    pthread_mutex_unlock(&mut);
+    
     void *thread_result;
-
 
     //creation
     res = pthread_create(&t1, NULL, threadT1, (void *) &val);
@@ -88,7 +104,6 @@ int main(int argc, char** argv) {
     }
     printf("attente de la fin du thread...\n");
 
-
     //utilisation
     val.v1 = 10;
     val.v2 = 20;
@@ -98,7 +113,11 @@ int main(int argc, char** argv) {
         perror("Thread join a foir... echoué");
         exit(EXIT_FAILURE);
     }
+    
+    pthread_mutex_lock(&mut);
     printf("valeur : %d, %d, %d\n", zoneCommune.v1, zoneCommune.v2, zoneCommune.v3);
+    pthread_mutex_unlock(&mut);
+    
     val.v1 = 40;
     val.v2 = 50;
     val.v3 = 60;
@@ -107,7 +126,11 @@ int main(int argc, char** argv) {
         perror("Thread join a foir... echoué");
         exit(EXIT_FAILURE);
     }
+    
+    pthread_mutex_lock(&mut);
     printf("valeur : %d, %d, %d\n", zoneCommune.v1, zoneCommune.v2, zoneCommune.v3);
+    pthread_mutex_lock(&mut);
+    
     val.v1 = 70;
     val.v2 = 80;
     val.v3 = 90;
@@ -117,6 +140,7 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
     printf("valeur : %d, %d, %d\n", zoneCommune.v1, zoneCommune.v2, zoneCommune.v3);
+
     exit(EXIT_SUCCESS);
     return (EXIT_SUCCESS);
 }
